884e262acb453de68ae5ce1a021329e83257d19f
Zoltan Hari <harizozi1009@gmail.com>
2025-11-05 09:14:47 -0700

    Add Comments

--- jane_the_ripper.py
+++ jane_the_ripper.py
@@ -3,6 +3,7 @@ import hashlib
 import sys 
 import time
 
+# Function to simulate typing effect instead of just printing it all at once
 def type_out(text, delay=0.03):
     for char in text:
         sys.stdout.write(char)
@@ -10,17 +11,33 @@ def type_out(text, delay=0.03):
         time.sleep(delay)
     print()
 
+# Bash colors for output to organize cracked and failed hashes
 RED = '\033[91m'
 GREEN = '\033[92m'
 RESET = '\033[0m'
 
 def crack_passwords(wordlist_path, hash_file_path, hash_type):
+    """
+    Crack passwords from a wordlist that match the given hashes
+    
+    ARGS: 
+        wordlist_path (str): Path to the wordlist file
+        hash_file_path (str): Path to the file containing hashes
+        hash_type (str): Type of hash (md5, sha1, sha256)
+
+    Returns:
+        cracked (dict): A dictionary with hashes as keys and cracked passwords as values
+        hashes_list (list): A list of all hashes from the hash file
+
+    """
+
     hashes_list = []
     with open(hash_file_path, "r") as hashes:
         for line in hashes:
             hash = line.strip()
             if hash:
                 hashes_list.append(hash)
+    # I used a set because it can instantly find any item instead of going through every item in a list one by one
     hashes_set = set(hashes_list)
     cracked = {}
     with open(wordlist_path, "r") as wordlist:
@@ -43,8 +60,10 @@ def crack_passwords(wordlist_path, hash_file_path, hash_type):
                             if hash in hashes_set and hash not in cracked:
                                 cracked[hash] = password
 
+    # Returned list instead of set to maintain order of hashes from the file
     return cracked, hashes_list
 
+# Added error handling outside of main function for better testability
 def validate_path_to_files(file):
     try:
         with open(file, "r"):
